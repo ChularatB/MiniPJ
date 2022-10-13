4598436a70f6030cb9752823a23f1681d87b8da9
ChularatB <91866956+ChularatB@users.noreply.github.com>
2022-10-13 21:36:23 +0700

    Update delete1.php

--- mini_project/delete1.php
+++ mini_project/delete1.php
@@ -3,9 +3,8 @@ $pdo = new PDO("mysql:host=localhost; dbname=funtasy; charset=utf8", "root", "")
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 <?php
-	$stmt = $pdo->prepare("DELETE FROM `ใบนัด` WHERE `รหัสใบนัด`=?");
+	$stmt = $pdo->prepare("DELETE FROM ใบนัด WHERE รหัสใบนัด=?");
 	$stmt->bindParam(1, $_GET["รหัสใบนัด"]); 
 	if ($stmt->execute()) 
-	    header("location: confirm.php");
-
+	    header("location: cancle.php");
 ?>
